e9f5f8d879d83f4acefd507983ed05cb6259fece
NeonD00m <neond00m.business@gmail.com>
2024-02-09 14:38:05 -0800

    added safe area and made it use color scheme

--- App.js
+++ App.js
@@ -1,139 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-
-function Square({ value, onSquareClick }) {
-  return (
-    <Button title={value} className="square" onClick={onSquareClick}>
-      <Text>{value}</Text>
-    </Button>
-  );
-}
-
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    onPlay(nextSquares);
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
-
-  return (
-    <>
-      <View className="status"><Text>{status}</Text></View>
-      <View className="board-row" style={{flexDirection: 'row'}}>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </View>
-      <View className="board-row" style={{flexDirection: 'row'}}>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </View>
-      <View className="board-row" style={{flexDirection: 'row'}}>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </View>
-    </>
-  );
-}
-
-function Game() {
-  const [history, setHistory] = useState([Array(9).fill("")]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
-
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
-
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
-
-  // const moves = history.map((squares, move) => {
-  //   let description;
-  //   if (move > 0) {
-  //     description = 'Go to move #' + move;
-  //   } else {
-  //     description = 'Go to game start';
-  //   }
-  //   return (
-  //     <li key={move}>
-  //       <Button onClick={() => jumpTo(move)}>{description}</Button>
-  //     </li>
-  //   );
-  // });
-
-  return (
-    <View className="game">
-      <View className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </View>
-      {/* <View className="game-info">
-        <ol>{moves}</ol>
-      </View> */}
-    </View>
-  );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+import { StyleSheet, Text, View, Button, Appearance, useColorScheme } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Navigation from './components/Navigation';
 
 export default function App() {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
   return (
-    <View style={styles.container}>
-      <View style={{justifyContent: "center", alignContent: "center"}}>
-        <Game />
-      </View>
-      {/* <Text>cdyjgvk!</Text>
-      <StatusBar style="auto" /> */}
+    <View style={[styles.container, themeContainerStyle]}>
+    <Text style={[styles.text, themeTextStyle]}>
+      I want to be OFF THE SCREEN OR COVERED BY A CAMERA
+    </Text>
+      <SafeAreaView style={[styles.container, themeContainerStyle]}>
+        <Text style={[styles.text, themeTextStyle]}>
+          yo wassup I am hopefully not off the screen
+        </Text>
+        <Button title="idk press me or something" style={[styles.text, themeTextStyle]}>
+        </Button>
+        {/* <View style={{justifyContent: "center", alignContent: "center"}}>
+          
+        </View>
+        <Text>cdyjgvk!</Text>
+        <StatusBar style="auto" /> */}
+        {/* <Navigation /> */}
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    // flex: 1,
     // alignItems: 'center',
     // justifyContent: 'center',
+  },
+  lightContainer: {
+    backgroundColor: '#d0d0c0',
+  },
+  darkContainer: {
+    backgroundColor: '#242c40',
+  },
+  lightThemeText: {
+    color: '#242c40',
+  },
+  darkThemeText: {
+    color: '#d0d0c0',
   },
 });
