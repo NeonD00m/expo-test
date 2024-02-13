7ec7f788e8ff24b35555c86fce72c9d6c4b1c03e
NeonD00m <neond00m.business@gmail.com>
2024-02-13 15:31:35 -0800

    revamped to use components

--- screens/Data.js
+++ screens/Data.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, Button, Appearance, useColorScheme } from 'react-native';
-import { LineChart } from 'react-native-chart-kit';
+import { DatapointFrame } from './components/DatapointFrame';
 import React from 'react';
 
 export default function Data() {
@@ -15,30 +15,8 @@ export default function Data() {
 
     return (
       <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-        <Text style={{fontSize: 24}}>
-          Your stock
-        </Text>
-        <LineChart
-          data={line}
-          width={400}
-          height={300}
-          yAxisLabel={'$'}
-          chartConfig={{
-            backgroundColor: useColorScheme() === 'light' ? '#ffffff' : '#000000',
-            backgroundGradientFrom: '#fb8c00',
-            backgroundGradientTo: '#ffa726',
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
-              borderRadius: 16
-            }
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
-            borderRadius: 16
-          }}
-        />
+        {/* put a scrolling list of favorited stats */}
+        <DatapointFrame name="Round Square Ltd." graphData={line} />
       </View>
     )
 }
