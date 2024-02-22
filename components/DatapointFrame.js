103d1f780b5686589dc6f6f68da6a98f935f1961
NeonD00m <neond00m.business@gmail.com>
2024-02-22 11:30:55 -0800

    not working, updated for new GraphDisplay

--- components/DatapointFrame.js
+++ components/DatapointFrame.js
@@ -8,25 +8,38 @@ import { StyleSheet, Text, View, Button, Appearance, useColorScheme } from 'reac
 import GraphDisplay from './GraphDisplay'
 import React from 'react';
 
-export default function DatapointFrame({name, graphData, openWindow}) {
-    return (
-        <View style={{borderRadius: 20, aspectRatio: 1.5, height: "100%"}} onTouchEnd={(event) => {
-            openWindow(name, graphData, data);
-        }}>
-            <GraphDisplay graph={graphData} />
-            <Text title={name} style={styles.title} />
-            <View style={{flex: 1, width: "100%"}}>
-                {/* put tags? */}
-            </View>
-        </View>
-    );
+export default function DatapointFrame({name, graphData, data, openWindow}) {
+  const self = React.useRef()
+  return (
+    <View ref={self} style={styles.frame} onTouchEnd={(event) => {
+        openWindow(name, graphData, data);
+    }}>
+      <GraphDisplay graph={graphData} parent={self}
+        width={self.current && self.current.offsetWidth ? self.current.offsetWidth : null}
+        height={self.current && self.current.offsetHeight ? self.current.offsetHeight : null}
+      />
+      <Text style={styles.title}>
+        {name}
+      </Text>
+      <View style={{flex: 1, width: "100%"}}>
+        {/* put tags? */}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   title: {
-    width: "100%",
+    width: "100%", height: 26,
     position: "absolute", top: 0,
     fontSize: 16,
-    backgroundColor: "#ffffff", color: "#000000"
+    backgroundColor: "#ffffff", color: "#000000",
+    borderTopLeftRadius: 20, borderTopRightRadius: 20,
+    paddingLeft: 32,
   },
+  frame: {
+    backgroundColor: "#ffffff", 
+    borderRadius: 20, marginRight: 20,
+    aspectRatio: 1.5, height: "100%"
+  }
 });
