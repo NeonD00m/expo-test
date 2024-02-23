464015df98cadba5c80002d8295f1e2d6d3785ab
NeonD00m <neond00m.business@gmail.com>
2024-02-23 14:35:17 -0800

    favorite icon + similar carousel in progress

--- components/DataWindow.js
+++ components/DataWindow.js
@@ -3,10 +3,30 @@ import { StyleSheet, Text, View, Button, Modal, useColorScheme, ScrollView } fro
 import { useSafeAreaFrame, useSafeAreaInsets, SafeAreaView } from 'react-native-safe-area-context';
 import GraphDisplay from './GraphDisplay';
 import React from 'react';
+import { Entypo } from '@expo/vector-icons';
+import Carousel from './Carousel';
+import DatapointFrame from './DatapointFrame';
+
+const iconColor = {
+  light: {
+    true: "#fff200",
+    false: "#808080",
+  },
+  dark: {
+    true: "#fff200",
+    false: "#808080",
+  },
+};
+
+const iconImage = {
+  true: "star",
+  false: "star-outlined",
+}
 
 export default function DataWindow({visible, toggle, state}) {
   const insets = useSafeAreaInsets();
   const frame = useSafeAreaFrame();
+  const [favorited, setFavorited] = React.useState(false);
   return (
     <Modal 
       animationType="slide"
@@ -14,34 +34,57 @@ export default function DataWindow({visible, toggle, state}) {
     >
       <SafeAreaView style={styles.background}>
         <View style={styles.header}>
-          <Text style={styles.title}>{state.name}</Text>
           <Button title="< back" style={styles.backButton} onPress={()=>{toggle(!visible)}}/>
+          <Text style={styles.title}>{state.name}</Text>
+          <Button style={styles.favButton} title="" onPress={()=>{
+            console.log(`${favorited ? "un" : ""}favorited ${state.name}`);
+            setFavorited(!favorited);
+          }}>
+            <Entypo name={iconImage[favorited.toString()]} size={24} color={iconColor[favorited.toString()]} />
+          </Button>
         </View>
         <GraphDisplay graph={state.graph} />
         <Text>
           pretend there is more data here
         </Text>
+        {/* <Text style={styles.Section}>
+          Similar Statistics
+        </Text> */}
+        <Carousel title="Similar Statistics">
+          <DatapointFrame graphData={state.Data}/>
+        </Carousel>
       </SafeAreaView>
     </Modal>
   );
 }
 
 const styles = StyleSheet.create({
+  favButton: {
+    marginHorizontal: "5%",
+    width: "20%",
+  },
   backButton: {
     background: "rgba(0, 0, 0, 1)",
-    position: "absolute", left: 0, width: "30%"
+    position: "absolute", left: 0, width: "30%",
+    alignItems: "center", justifyContent: "flex-start",
   },
   title: {
     // width: "100%",
     textAlign: "center",
     fontSize: 32,
   },
+  section: {
+    fontSize: 24,
+  },
   header: {
+    flex: 0,
+    alignItems: "center",
+    // justifyContent: "flex-start",
     width: "100%",
     borderBottomWidth: 2, borderBottomColor: "#d1d1d1",
-    // alignItems: "center",
-    // justifyContent: "center",
-    // alignContent: "center",
+      // alignItems: "center",
+      justifyContent: "space-between",
+      // alignContent: "center",
     flexDirection: "row"
   },
   background: {
