78aceaf4b55e17425eda0cb9941c4cbfe54c5bb6
NeonD00m <neond00m.business@gmail.com>
2024-02-29 10:54:36 -0800

    attempted settings horribly

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,43 +1,46 @@
-import { StyleSheet, Text, View, Button, Appearance, useColorScheme, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, useColorScheme, TextInput, Switch } from 'react-native';
 import React from 'react';
+import { useSettings, getDefaultSettings } from '../modules/UseSettings';
 
-export default function Settings({curSettings, setSettings}) {
-  // const[curSettings, setSettings] = React.useState({})
-  const [text, setText] = React.useState('');
-  function onChangeText(newText) {
-    console.log(newText);
-    setText(newText);
-  }
+export default function Settings() {
+  let [curSettings, setSettings] = useSettings();
+  // React.useState([
+  //   ["Override Device Color Theme", false],
+  //   ["Override Dark Mode", true, (settings) => {return settings[1][2]}],
+  //   ["Override Name", "DEFAULT"],
+  // ]);
 
-  let children = curSettings.map((value, index) => {
-
+  let children = curSettings.map(([text, value, dependency], index) => {
     //TODO: use value[3] function to check whether this will be enabled
-
+    console.log(value)
     return (
-      <View style={styles.settingRow}>
+      <View style={styles.settingRow} key={index}>
         <Text style={styles.rowText}>
-          {value[0]}
+          {text}
         </Text>
         {value === true || value === false ? (
           <Switch
+            disabled={dependency ? dependency(curSettings) : false}
             trackColor={{false: '#767577', true: '#81b0ff'}}
-            thumbColor={value[1] ? '#f5dd4b' : '#f4f3f4'}
+            thumbColor={value ? '#f5dd4b' : '#f4f3f4'}
             ios_backgroundColor="#3e3e3e"
-            onValueChange={() => {
-              curSettings[index][1] = !value[1];
+            onValueChange={(newValue) => {
+              console.log(`Switch: ${newValue}`)
+              curSettings[index][1] = newValue;//!value[1];
               setSettings(curSettings);
             }}
-            value={value[1]}
+            value={value}
           />
         ) : (
           <TextInput
             onChangeText={(newText) => {
-              console.log(newText);
+              console.log(`Text: ${newText}`)
               curSettings[index][1] = newText;
               setSettings(curSettings);
             }}
-            value={value[1]}
-            placeholder={value[1]}
+            value={value}
+            placeholder="NO VALUE"
+            // keyboardType='default'
           />
         )}
       </View>
@@ -45,22 +48,28 @@ export default function Settings({curSettings, setSettings}) {
   })
   
   return (
-    <ScrollView style={styles.settingsContainer}>
-      <Text>
-          idk what kinda settings would even be here
-      </Text>
-      <TextInput
-        onChangeText={onChangeText}
-        value={text}
-        placeholder="type a message"
-      />
+    <ScrollView style={styles.scrollView} contentContainerStyle={styles.settingsContainer}>
+      <View style={styles.settingRow}>
+        <Text style={styles.rowText}>
+          SETTINGS
+        </Text>
+        <Button title="RESET SETTINGS" onPress={() => {setSettings(getDefaultSettings())}} />
+      </View>
+      {children}
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   settingRow: {
-    width: "100%",
+    width: "100%", height: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  scrollView: {
+    flex: 1,
   },
   settingsContainer: {
     flex: 1,
@@ -68,6 +77,6 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   rowText: {
-    fontSize: 14,
+    fontSize: 16,
   },
 });
